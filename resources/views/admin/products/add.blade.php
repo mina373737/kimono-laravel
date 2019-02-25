753ca60671788b4286d16393d966569997c8c6e8
Minami Akaishi <akaishi.minani@gmail.com>
2019-02-24 22:50:59 -0700

    画像アップロード機能を試しに追加しました

--- resources/views/admin/products/add.blade.php
+++ resources/views/admin/products/add.blade.php
@@ -11,6 +11,17 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>販売商品の新規追加します</h2>
+                <form action="#" method="post">
+                  <p>商品名:<br>
+                  <input type="text" name="name"></p>
+                  <p>税込価格:<br>
+                  <input type="text" name="price"></p>
+                  <p>商品写真:<br>
+                    {{ Form::file('thefile') }}
+                  <input name="thefile" type="file">
+                  {{Form::file('thefile', ['class' => 'field'])}}
+                  <input class="field" name="thefile" type="file">
+                </form>
             </div>
         </div>
     </div>
